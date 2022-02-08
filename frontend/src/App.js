6364c77f29bd9799c600c0733a6c6edc300d7d85
bsluther <briansluther@gmail.com>
2022-02-08 12:18:01 -0700

    Basic server that serves the built frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <section className='grid grid-rows-container'>
+      <section className='flex row-start-1 w-full border border-black justify-center'>
+        <div className=''>Header</div>
+      </section>
+      <div className='row-start-2'>Body</div>
+    </section>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        'container': '5rem auto'
+      }
+    },
+  },
+  plugins: [],
+}
