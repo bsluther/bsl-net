d01d0d4c11bdf15388b7c0fd1b786f4e46055d0d
bsluther <briansluther@gmail.com>
2022-02-08 18:59:51 -0700

    ...

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const server = express()
+const PORT = process.env.PORT || 3000
+
+server.use(
+  express.static(
+    path.join(__dirname,'../frontend/build'))
+)
+
+server.get('/', (req, res) => {
+  res.sendFile(
+      path.join(__dirname, '../frontend/build/index.html')
+  )
+})
+
+server.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`)
+})
